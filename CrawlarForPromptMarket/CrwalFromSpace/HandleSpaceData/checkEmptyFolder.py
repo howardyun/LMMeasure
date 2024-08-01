c670e85c634b0b2908cc1dd811bd6cf6598df89d
基于深度学习的DGA恶意域名检测 <t2024100131138@eduxiji.net>
2024-08-01 09:28:40 +0800

    add file

--- CrawlarForPromptMarket/CrwalFromSpace/HandleSpaceData/checkEmptyFolder.py
+++ CrawlarForPromptMarket/CrwalFromSpace/HandleSpaceData/checkEmptyFolder.py
@@ -1,16 +1,16 @@
 import os
-import shutil
 
-def remove_empty_folders(path):
+def remove_empty_first_level_folders(path):
     # 检查路径是否存在
     if not os.path.exists(path):
         print(f"Path {path} does not exist.")
         return
 
-    # 遍历文件夹
-    for root, dirs, files in os.walk(path, topdown=False):
-        for dir_name in dirs:
-            dir_path = os.path.join(root, dir_name)
+    # 遍历第一级子文件夹
+    for dir_name in os.listdir(path):
+        dir_path = os.path.join(path, dir_name)
+        # 确保是文件夹
+        if os.path.isdir(dir_path):
             # 如果文件夹为空，删除它
             if not os.listdir(dir_path):
                 print(f"Removing empty folder: {dir_path}")
@@ -18,4 +18,4 @@ def remove_empty_folders(path):
 
 # 使用示例
 folder_path = '/Users/howardyun/Desktop/workspace/cloned_repos'
-remove_empty_folders(folder_path)
+remove_empty_first_level_folders(folder_path)
